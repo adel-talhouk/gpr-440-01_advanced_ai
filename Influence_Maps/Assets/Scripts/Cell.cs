55f6d063eff9b11780400e11cc9904f76e68a49a
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2021-03-28 12:16:07 -0400

    Creting grid successfully. Cost displays using gizmos. Default cost is 0f. Going to work on towers and influence.

---
+++ Influence_Maps/Assets/Scripts/Cell.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class Cell : MonoBehaviour
+{
+    public float cost = 0f;
+
+    //Zombies in the cell
+    int zombiesInCell = 0;
+    public int ZombiesInCell {  get { return zombiesInCell; } }
+
+    public void IncreaseCost(float increaseAmount)
+    {
+        cost += increaseAmount;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Zombie"))
+            zombiesInCell++;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Zombie"))
+            zombiesInCell--;
+    }
+
+    void OnDrawGizmos()
+    {
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.alignment = TextAnchor.MiddleCenter;
+        Handles.Label(transform.position, cost.ToString(), style);
+    }
+}
--- Influence_Maps/Assets/Scripts/GridManager.cs
+++ Influence_Maps/Assets/Scripts/GridManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public GameObject cellPrefab;
+
+    [Header("Grid Data")]
+    public Vector2Int gridSize;
+    [Range(0.1f, 0.5f)] public float cellSpacing = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void CreateGrid()
+    {
+        //For through the 2D array
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                //Spawn cell at (i, j) with offset
+                Vector2 spawnPos = new Vector2(i + cellSpacing, j + cellSpacing);
+                GameObject cell = Instantiate(cellPrefab, spawnPos, Quaternion.identity, transform);
+                cell.GetComponent<Cell>().cost = 0;
+            }
+        }
     }
 }
